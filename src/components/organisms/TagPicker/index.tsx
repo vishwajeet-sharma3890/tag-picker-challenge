ab60659db378e7518958fc321515eaa8ea205b7d
Vishwajeet Sharma <vishwajeet.sharma3890@gmail.com>
2021-02-28 20:13:05 +1000

    Fixed autofocus, slow response issues

--- src/components/organisms/TagPicker/index.tsx
+++
@@ -1,6 +0,0 @@
-import { TagPickerProps } from "./types";
-import Tags from "../../molecules/Tags";
-
-export default function TagPicker({ userTags, allTags }: TagPickerProps) {
-  return <Tags allTags={allTags} userTags={userTags} />;
-}
--- src/components/organisms/TagPicker/types.ts
+++
@@ -1,6 +0,0 @@
-import { TagData } from "../../../contexts/UserContext/types";
-
-export interface TagPickerProps {
-  userTags: TagData[];
-  allTags: TagData[];
-}
